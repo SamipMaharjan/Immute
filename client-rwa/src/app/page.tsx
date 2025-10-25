7f2a36d1770bd2dd5408f0e12313dc041d415aad
Leeja Shrestha <leeja.shrestha@bajratechnologies.com>
2025-10-25 11:36:22 +0545

    feat:Unpolished navbar

--- client-rwa/src/app/page.tsx
+++ client-rwa/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>hello owl
-      <HomePage />
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
